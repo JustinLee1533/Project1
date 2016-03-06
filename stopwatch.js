9ffb6dcbb4ae8b8676437b204d12ab0734c2992d
Brandon Givens <brandon.a.givens@gmail.com>
2016-03-06 10:05:22 -0600

    Add code to explain new timing algorithm in stopwatch.js Fix issue where stopwatch display would occasionally skip seconds because it was only updating every second. It was still accurate, though Add label to stopwatch display denoting it as the stopwatch display

--- stopwatch.js
+++ stopwatch.js
@@ -17,19 +17,25 @@ function Stopwatch()
 	if(stopwatch_running == true)
 	{
 		var temp = sw_milliseconds_1970;
+		//Updating number of milliseconds since 1970
 		sw_milliseconds_1970 = Date.now();
+		//Computing time difference between now and last execution
 		var time_diff = sw_milliseconds_1970 - temp;
+		//Incrementing number of milliseconds, and updating other timing variables
 		sw_milliseconds = sw_milliseconds + time_diff;
 		sw_seconds = Math.floor(sw_milliseconds/1000);
 		sw_minutes = Math.floor(sw_seconds/60);
 		sw_hours = Math.floor(sw_minutes/60);
-	
+
+		//Formating time variables to show two digits	
 		var seconds = (sw_seconds%60 < 10 ? "0" : "") + sw_seconds%60;
 		var minutes = (sw_minutes%60 < 10 ? "0" : "") + sw_minutes%60;
 		var hours = (sw_hours%24 < 10 ? "0" : "") + sw_hours%24;
+		//Forming string to display time in HTML
 		var currentTime = hours + ":" + minutes + ":" + seconds;
 
-		document.getElementById("Stopwatch").firstChild.nodeValue = currentTime;
+		//Updating HTML
+		document.getElementById("Stopwatch").firstChild.nodeValue = "Stopwatch: "+currentTime;
 	}
 	else if(stopwatch_running == false)
 	{
@@ -52,6 +58,6 @@ function change_stopwatch_mode()
 function executeStopwatch() {
     stopwatch_running = true;
     sw_milliseconds_1970 = Date.now();
-    setInterval(Stopwatch, 1000);
+    setInterval(Stopwatch, 250);
     Stopwatch();
 }
