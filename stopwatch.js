8ab86cd1b8793a224be5ae93e66585285010f78d
Brandon Givens <brandon.a.givens@gmail.com>
2016-03-06 09:51:26 -0600

    Fix issue with stopwatch not updating time correctly when the resume button is pressed. Required rework of timing algorithm

--- stopwatch.js
+++ stopwatch.js
@@ -1,27 +1,48 @@
 var stopwatch_running = false;
+//Variables to keep track of pause time
 var pause_hours = 0;
 var pause_minutes = 0;
 var pause_seconds = 0;
 var pause_milliseconds = 0;
 
+//Variables to keep track of current stopwatch timing
+var sw_milliseconds_1970 = 0;
+var sw_milliseconds = 0;
+var sw_seconds = 0;
+var sw_minutes = 0;
+var sw_hours = 0;
+
 function Stopwatch()
 {
 	if(stopwatch_running == true)
 	{
-		var stopwatch_Time = new Date(2016, 0, 0, 0, 0, 0, 0);
-		var diffTime = new Date() - startTime;
-		stopwatch_Time.setMilliseconds(stopwatch_Time.getMilliseconds() + diffTime);
-
-		var seconds = (stopwatch_Time.getSeconds() < 10 ? "0" : "") + stopwatch_Time.getSeconds();
-		var minutes = (stopwatch_Time.getMinutes() < 10 ? "0" : "") + stopwatch_Time.getMinutes();
-		var hours = (stopwatch_Time.getHours() < 10 ? "0" : "") + stopwatch_Time.getHours();
+		var temp = sw_milliseconds_1970;
+		sw_milliseconds_1970 = Date.now();
+		var time_diff = sw_milliseconds_1970 - temp;
+		sw_milliseconds = sw_milliseconds + time_diff;
+		sw_seconds = Math.floor(sw_milliseconds/1000);
+		sw_minutes = Math.floor(sw_seconds/60);
+		sw_hours = Math.floor(sw_minutes/60);
+	
+		var seconds = (sw_seconds%60 < 10 ? "0" : "") + sw_seconds%60;
+		var minutes = (sw_minutes%60 < 10 ? "0" : "") + sw_minutes%60;
+		var hours = (sw_hours%24 < 10 ? "0" : "") + sw_hours%24;
 		var currentTime = hours + ":" + minutes + ":" + seconds;
 
 		document.getElementById("Stopwatch").firstChild.nodeValue = currentTime;
 	}
 	else if(stopwatch_running == false)
 	{
-		//startTime = new Date();
+		sw_milliseconds_1970 = Date.now();
+		//var temp_date = new Date();
+		//var temp_ms = temp_date.getMilliseconds() - pause_milliseconds;
+		//var temp_s = temp_date.getSeconds() - pause_milliseconds;
+		//var temp_m = temp_date.getMinutes() - pause_minutes;
+		//var temp_h = temp_date.getHours() - pause_hours;
+		//pause_milliseconds = pause_milliseconds + temp_ms;
+		//pause_seconds = pause_seconds + temp_s;
+		//pause_minutes = pause_minutes + temp_m;
+		//pause_hours = pause_hours + temp_h;
 	}
 }
 
@@ -30,24 +51,24 @@ function change_stopwatch_mode()
 	if(stopwatch_running == true)
 	{
 		stopwatch_running = false;
-		pause_milliseconds = startTime.getMilliseconds();
-		pause_seconds = startTime.getSeconds();
-		pause_minutes = startTime.getMinutes();
-		pause_hours = startTime.getHours();
+		//pause_milliseconds = startTime.getMilliseconds();
+		//pause_seconds = startTime.getSeconds();
+		//pause_minutes = startTime.getMinutes();
+		//pause_hours = startTime.getHours();
 	}
 	else if(stopwatch_running == false)
 	{
 		stopwatch_running = true;
-		startTime.setMilliseconds(pause_milliseconds);
-		startTime.setSeconds(pause_seconds);
-		startTime.setMinutes(pause_minutes);
-		startTime.setHours(pause_hours);
+		//startTime.setMilliseconds(pause_milliseconds);
+		//startTime.setSeconds(pause_seconds);
+		//startTime.setMinutes(pause_minutes);
+		//startTime.setHours(pause_hours);
 	}
 }
 
 function executeStopwatch() {
     stopwatch_running = true;
-    startTime = new Date();
+    sw_milliseconds_1970 = Date.now();
     setInterval(Stopwatch, 1000);
     Stopwatch();
 }
